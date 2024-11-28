5830e27a719ae65d666f2989cdc5d9bd1b3cbfb3
Yuliana-Verdugo <168777045+Yuls18@users.noreply.github.com>
2024-11-27 21:43:31 -0800

    Add files via upload

---
+++ Base De Datos/libros_db.sql
@@ -0,0 +1,24 @@
+CREATE DATABASE libros_db;
+
+use libros_db;
+
+CREATE TABLE autores (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE editoriales (
+    id_editorial INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_editorial VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE libros_db (
+    id_libro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    id_autor INT,
+    id_editorial INT,
+    formato VARCHAR(50),
+    precio DECIMAL(10, 2),
+    FOREIGN KEY (id_autor) REFERENCES autores(id_autor),
+    FOREIGN KEY (id_editorial) REFERENCES editoriales(id_editorial)
+);
